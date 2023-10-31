795c2af2812eb29a5074b139cec2a9332838d8da
Md Adil Khan <mdadilakhtar8@gmail.com>
2023-10-31 17:04:39 +0530

    origin updated

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ import cors from 'cors'
 import bodyParser from 'body-parser';
 
 import cookieParser from 'cookie-parser';
-
+const corsOptions = {
+    origin: 'https://advacnetodoapp.web.app',
+    credentials: true,
+  };
 
 env.config()
 const app=express();
 
 app.use(cookieParser())
-app.use(cors({credentials:true, origin:"*"}))
+app.use(cors(corsOptions))
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 
